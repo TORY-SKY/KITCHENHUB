6299ab2b26167f8a1892cb0400c24a595fb87886
TORY-SKY <ezevictor559@gmail.com>
2024-04-01 12:41:04 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Homepage from "./components/Homepage";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Homepage />
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h3>I just want to be successful and holy</h3>
+    </div>
+  );
+};
+
+export default Homepage;
