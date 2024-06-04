fed1609274965b0450f77b23946575f3f617db18
TORY-SKY <ezevictor559@gmail.com>
2024-06-04 12:34:19 +0100

    we're making progress

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Homepage = () => {
   return <div>
-    <h1>welcome to the home page</h1>
+    <h1 className="Welcome-Phrase">welcome to the home page</h1>
   </div>;
 };
 
