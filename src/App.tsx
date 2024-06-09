8504064320516d64234cad51ab1ced654ac05d04
TORY-SKY <ezevictor559@gmail.com>
2024-06-09 17:21:51 +0100

    react

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { useState } from "react";
-import Homepage from "./components/Homepage";
-import Header from "./components/Header";
-
 function App() {
-  return (
-    <>
-      <Header />
-      <Homepage />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/components/Homepage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Homepage = () => {
-  return <div>
-    <h1 className="Welcome-Phrase">welcome to the home page</h1>
-  </div>;
-};
-
-export default Homepage;
