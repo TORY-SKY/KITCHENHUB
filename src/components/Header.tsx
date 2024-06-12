6a01f78abf7a943b00f4f73d68b393b89f4890e6
TORY-SKY <ezevictor559@gmail.com>
2024-06-12 11:16:50 +0100

    Victory

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Linkss = () => {
+  return <h1>This is the lin tag bro</h1>;
+};
+const Header = () => {
+  return (
+    <div>
+      <Linkss />
+    </div>
+  );
+};
+
+export default Header;
